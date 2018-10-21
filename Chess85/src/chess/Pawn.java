ad5be7dd1258a3957b97dbe4a059ed8ad7607bd0
Vincent Phan <vincent.phan@rutgers.edu>
2018-10-21 15:41:09 -0400

    4 updates

--- Chess85/src/chess/Pawn.java
+++ Chess85/src/chess/Pawn.java
@@ -5,4 +5,48 @@ public class Pawn extends Piece{
 		this.color = color;
 		this.name = name;
 	}
+	
+	boolean validMove(int x1, int x2, int y1, int y2) {
+		//if pawn havn't move yet: can move 2 spaces, 1 space, or 1 space diagonal to kill
+		if(!moveYet(x1)) {
+			if(x1 == x2 && y2 == y1+2) {
+				return true;
+			}
+			else if(x1 == x2 && y2 == y1+1) {
+				return true;
+			}
+			//this returns true only if there is a pawn of opposite color on its foward diagnonal.
+			//this is a legal move, but leave the check on the main to find out if pawn is there to kill?
+			else if(x1 == x2-1 && y2 == y1+1) {
+				return true;
+			}
+			else {
+				return false;
+			}
+		}
+		//pawn already moved
+		else {
+			if(x1 == x2 && y2 == y1+1 ) {
+				return true;
+			}
+			else if(x1 == x2-1 && y2 == y1+1) {
+				return true;
+			}
+			else {
+				return false;
+			}
+		}
+	}
+	
+	public boolean moveYet(int x1) {
+		if(name.equalsIgnoreCase("wp") && x1 == 6) {
+			return false;
+		}
+		else if(name.equalsIgnoreCase("bp") && x1 == 1) {
+			return false;
+		}
+		else {
+			return true;
+		}
+	}
 }
--- Chess85/src/chess/Queen.java
+++ Chess85/src/chess/Queen.java
@@ -1,8 +1,18 @@
 package chess;
 
 public class Queen extends Piece{
-public Queen(boolean color, String name) {
+	public Queen(boolean color, String name) {
 		this.color = color;
 		this.name = name;
+	}
+	
+	boolean validMove(int x1, int x2, int y1, int y2) {
+		//if movement is diagonal, horizontal, or vertical
+		if((x1 - y1) == (x2 - y2) || (x1 + y1) == (x2 + y2) || (x1 == x2) || (y1 == y2)) {
+			return true;
+		}
+		else {
+			return false;
+		}
 	}
 }
