97ab0a040739a98eb3f9b7dec459b824e76cbfc9
Vincent Phan <vincent.phan@rutgers.edu>
2018-10-20 21:39:37 -0400

    piece and bishop

--- Chess85/src/chess/Bishop.java
+++ Chess85/src/chess/Bishop.java
@@ -5,4 +5,13 @@ public class Bishop extends Piece {
 		this.color = color;
 		this.name = name;
 	}
+	
+	boolean validMove(int x1, int x2, int y1, int y2) {
+		if((x1 - y1) == (x2 - y2) || (x1 + y1) == (x2 + y2)) {
+			return true;
+		}
+		else {
+			return false;
+		}
+	}
 }
