4abdace7272c2ec23709f3e0360b688dd7d9350b
cl217 <cindy.lin217@gmail.com>
2018-10-21 01:40:22 -0400

    pathChecks, deleted BP, kn validM, start, convert changes

--- Chess85/src/chess/Chess.java
+++ Chess85/src/chess/Chess.java
@@ -1,48 +1,66 @@
 
 //MAIN CLASS
 package chess;
+
+import java.util.Scanner;
+
 public class Chess {
 	
 	public static void main(String[] args) {
 		Board b = new Board();
 		b.initialize();
 		b.print();
-		//start(b);
+		start(b);
 	}
 	
-	/*
+	
 	public static void start( Board board ) {
+		Scanner sc = new Scanner( System.in );
+		boolean wTurn = true; //true-white, false-black
+		String from = new String();
+		String to = new String();
+		String input = new String();
+		int[] moveCom = new int[4]; //moveCommands
+		while( /*game is still on aka not checkmate, draw, resign */){
+			if( wTurn ) {
+				System.out.println("White's turn: ");
+				wTurn = false;
+			}else {
+				System.out.println("Black's turn: ");
+				wTurn = true;
+			}
+			input = sc.nextLine();
+			//check type of input, see notes.txt
+			//error check
+			if( /* Regular input g7 g8 */) {
+				moveCom = convertArr(input); // moveCom[4] = x1, x1, x2, x2
+				
+			}
+			
 		
-		
+		}
 	}
-	*/
 	
-	public static int[] convert( String rcStr ) {
-		int[] rc = new int[2];
-		char row = rcStr.charAt(0);
-		char col = rcStr.charAt(2);
-		
-		switch(row) {
-			case '8': rc[0] = 0; break;
-			case '7': rc[0] = 1; break;
-			case '6': rc[0] = 2; break;
-			case '5': rc[0] = 3; break;
-			case '4': rc[0] = 4; break;
-			case '3': rc[0] = 5; break;
-			case '2': rc[0] = 6; break;
-			case '1': rc[0] = 7; break;
+	
+	public static int[] convertArr( String moveStr ) {
+		int[] coords = new int[4];
+		coords[0] = convert( moveStr.charAt(0) );
+		coords[1] = convert( moveStr.charAt(2) );
+		coords[2] = convert( moveStr.charAt(4) );
+		coords[3] = convert( moveStr.charAt(6) );
+		return coords;
+	}
+	public static int convert( char c ) {
+		switch(c) {
+			case '8': return 0; case 'a': return 0;
+			case '7': return 1; case 'b': return 1;
+			case '6': return 2; case 'c': return 2;
+			case '5': return 3; case 'd': return 3;
+			case '4': return 4; case 'e': return 4;
+			case '3': return 5; case 'f': return 5;
+			case '2': return 6; case 'g': return 6;
+			case '1': return 7; case 'h': return 7;
 		}
-		switch(col) {
-			case 'a': rc[1] = 0; break;
-			case 'b': rc[1] = 1; break;
-			case 'c': rc[1] = 2; break;
-			case 'd': rc[1] = 3; break;
-			case 'e': rc[1] = 4; break;
-			case 'f': rc[1] = 5; break;
-			case 'g': rc[1] = 6; break;
-			case 'h': rc[1] = 7; break;
-		}
-		
-		return rc;
+		return -1; //should never return this
 	}
 }
--- Chess85/src/chess/Knight.java
+++ Chess85/src/chess/Knight.java
@@ -5,4 +5,13 @@ public class Knight extends Piece {
 		this.color = color;
 		this.name = name;
 	}
+	
+	boolean validMove( int x1, int y1, int x2, int y2 ) {
+		int x = Math.abs(x2-x1);
+		int y = Math.abs(y2-y1);
+		if( (x==2 && y==1) || (x==1 && y==2) ) {
+			return true;
+		}
+		return false;
+	}
 }
