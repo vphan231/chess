0a29459145379698d9f08c2dff66e7cd77f5fc14
Vincent Phan <vincent.phan@rutgers.edu>
2018-10-21 15:51:16 -0400

    king

--- Chess85/src/chess/King.java
+++ Chess85/src/chess/King.java
@@ -8,35 +8,35 @@ public class King extends Piece {
 	
 	boolean validMove(int x1, int x2, int y1, int y2) {
 		//move up 1.
-		if(x1 == x2 || y2 == y1+1) {
+		if(x1 == x2 && y2 == y1+1) {
 			return true;
 		}
 		//move down 1
-		else if(x1 == x2 || y2 == y1-1) {
+		else if(x1 == x2 && y2 == y1-1) {
 			return true;
 		}
 		//move left 1
-		else if(x2 == x1-1 || y1 == y2) {
+		else if(x2 == x1-1 && y1 == y2) {
 			return true;
 		}
 		//move right 1
-		else if(x2 == x1+1 || y1 == y2) {
+		else if(x2 == x1+1 && y1 == y2) {
 			return true;
 		}
 		//move diagonal up left
-		else if(x2 == x1-1 || y2 == y1+1) {
+		else if(x2 == x1-1 && y2 == y1+1) {
 			return true;
 		}
 		//move diagonal up right
-		else if(x2 == x1+1 || y2 == y1+1) {
+		else if(x2 == x1+1 && y2 == y1+1) {
 			return true;
 		}
 		//move diagonal down left
-		else if(x2 == x1-1 || y2 == y1-1) {
+		else if(x2 == x1-1 && y2 == y1-1) {
 			return true;
 		}
 		//move diagonal down right
-		else if(x2 == x1+1 || y2 == y1-1) {
+		else if(x2 == x1+1 && y2 == y1-1) {
 			return true;
 		}
 		else {
